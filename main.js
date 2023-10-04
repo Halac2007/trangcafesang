fc6a0be227e02a293d46a18379572f617d11d7b6
Be Mai <bemai@gmail.com>
2023-10-04 22:07:38 +0700

    bootrap

---
+++ main.js
@@ -0,0 +1,28 @@
+fetch(`https://plo.vn/rss/giao-duc-21.rss`)
+  .then((response) => response.text())
+  .then((str) => new window.DOMParser().parseFromString(str, 'text/xml'))
+  .then((data) => {
+    console.log(data)
+
+    const items = data.querySelectorAll('item')
+    let html = ``
+    items.forEach((el) => {
+      console.log(el.querySelector('title').innerHTML.replace('<![CDATA[', '').replace(']]>', ''))
+
+      // console.log(el.querySelector('link').getAttribute('href'))
+      html += `
+      <article>
+      <a href="${el.querySelector('link').getAttribute('href')}">
+      <img src="${el.querySelector('image').innerHTML}" alt="">
+      </a>
+      <h2>
+      <a href="${el.querySelector('link').getAttribute('href')}">
+      ${el.querySelector('title').innerHTML.replace('<![CDATA[', '').replace(']]>', '')}
+      </a>
+      </h2>
+      </article>
+       
+      `
+    })
+    document.body.insertAdjacentHTML('beforeend', html)
+  })
